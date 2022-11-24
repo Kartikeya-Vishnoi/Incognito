eca9f83c07bf6ad183cc14d04feb36e796fd4aa5
Anukalp <aanujain1@gmail.com>
2022-11-24 22:20:51 +0530

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <div className='content'>
+                <p></p>
+                <p>Connect With Us</p>
+
+                <p>Give Your Startup's a Boom</p>
+                <p>"With Us gave company a new Wing" </p>
+                <button href='/' className='button'>Connect With Us</button>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
