a1bf8e1bf2edbd9796af7a5a5b90215d201c96c6
Kartikeya Vishnoi <kartikeya.vishnoi29@gmail.com>
2022-12-09 13:11:19 +0530

    Chanegd Firebase Configurations to Incognito

--- src/FireBase.js
+++ src/FireBase.js
@@ -9,13 +9,12 @@ import {getStorage} from "firebase/storage"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCrGZn94mlKGMjqmqAOnWDQS2UUE3Hp0C0",
-  authDomain: "motion-data-42bef.firebaseapp.com",
-  databaseURL: "https://motion-data-42bef-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "motion-data-42bef",
-  storageBucket: "motion-data-42bef.appspot.com",
-  messagingSenderId: "63672387572",
-  appId: "1:63672387572:web:b77233364256c52e9adba1"
+  apiKey: "AIzaSyC1Yjn1SxAxBsf-tlDvRTIuFR5k2MtShI8",
+  authDomain: "incognito-a80ac.firebaseapp.com",
+  projectId: "incognito-a80ac",
+  storageBucket: "incognito-a80ac.appspot.com",
+  messagingSenderId: "291379668227",
+  appId: "1:291379668227:web:1425ded4f528ccd3507536"
 };
 
 // Initialize Firebase
